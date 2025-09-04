5c2723b90ec3022c1a2cb7f20a2e072d0efd24b1
Kris <kvandreevskikh@gmail.com>
2025-09-05 00:06:31 +0300

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
 import './App.css';
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // убедитесь, что эта директория корректна
-    rollupOptions: {
-      external: ['@telegram-apps/sdk'],
-    },
-  },
-});
+    outDir: 'build',
+  }
+})
