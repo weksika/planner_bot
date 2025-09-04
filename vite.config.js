89c0dac0487c64f4603fb3cbd4e648219276e60e
Kris <kvandreevskikh@gmail.com>
2025-09-04 23:31:05 +0300

    h

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build', // убедитесь, что эта директория корректна
     rollupOptions: {
-      external: ['@telegram-apps/sdk'], // Исключаем модуль из автоматической обработки
+      external: ['@telegram-apps/sdk'],
     },
   },
-})
+});
